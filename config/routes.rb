4cae818c5b08f7282206a8f8b43165abdb80ca52
lim996 <simonestaffa96@gmail.com>
2018-07-25 15:37:47 +0200

    devise installed

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,14 @@
 Rails.application.routes.draw do
+  devise_for :users, controllers: {
+        sessions: 'users/sessions'
+      }
   get 'blogs/index'
- 
+
   resources :blogs do
   	resources :posts do
   		resources :comments
   	end
   end
- 
+
   root 'blogs#index'
 end
