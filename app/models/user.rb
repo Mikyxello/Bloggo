e34e8d34df74b7b9d8a32719eb465630edcb39bc
lim996 <simonestaffa96@gmail.com>
2018-08-01 15:35:24 +0200

    User image placeholder if no image is provided in the sign up form

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,7 +9,7 @@ class User < ApplicationRecord
 
 	validates_presence_of :name, :surname
 
-	validates_presence_of   :avatar_image
+	#validates_presence_of   :avatar_image
   validates_integrity_of  :avatar_image
   validates_processing_of :avatar_image
 
@@ -19,19 +19,33 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :trackable, :validatable,
 				 :omniauthable, :omniauth_providers => [:google_oauth2,:facebook]
 
-	 def self.from_omniauth(access_token)
-	   data = access_token.info
-	   user = User.where(:email => data["email"]).first
+	 #def self.from_omniauth(access_token)
+	  # data = access_token.info
+	   #user = User.where(:email => data["email"]).first
 
-	   unless user
-	     password = Devise.friendly_token[0,20]
-	     user = User.create(:name => data["first_name"],:surname => data["last_name"], email: data["email"],
-	       password: password, password_confirmation: password, avatar_image: data["image"]
-	     )
-			 user.save!
-	   end
-	   user
-	 end
+	   #unless user
+	     #password = Devise.friendly_token[0,20]
+	     #user = User.create(:name => data["first_name"],:surname => data["last_name"], email: data["email"],
+	      # password: password, password_confirmation: password, avatar_image: data["image"]
+	     #)
+			# user.save!
+	   #end
+	  # user
+	 #end
+
+	 def self.from_omniauth(auth)
+	  where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+	    user.email = auth.info.email
+	    user.password = Devise.friendly_token[0,20]
+	    user.name = auth.info.first_name   # assuming the user model has a name
+			user.surname = auth.info.last_name
+			user.avatar_image = auth.info.image
+			user.save!
+	    # If you are using confirmable and the provider(s) you use validate emails,
+	    # uncomment the line below to skip the confirmation emails.
+	    # user.skip_confirmation!
+	  end
+	end
 
 	 def self.new_with_session(params, session)
     super.tap do |user|
