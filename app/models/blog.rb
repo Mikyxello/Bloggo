737d2d0d964e36196b3669161638d261e2756da4
andreamisu <misu.it.official@gmail.com>
2018-08-09 15:23:23 +0200

    added Header / Profile pics in Bloggoes [now working] , Restyled bloggoes page, fixed post filters inside bloggoes page

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -6,9 +6,20 @@ class Blog < ApplicationRecord
 	is_impressionable :counter_cache => true, :column_name => :impressions_count
 	has_many :posts, dependent: :destroy
 
+	mount_uploader :header, ImageUploader
+	validates_integrity_of :header
+	validates_processing_of :header
+
+	mount_uploader :profile, ImageUploader
+	validates_integrity_of :profile
+	validates_processing_of :profile
+
 	belongs_to :user
 
 	validates :name, presence: true, length: { minimum: 5 }
+	validates :header, presence: true
+	validates :profile, presence: true
+
 
 	before_create do
 		self.suspended = false
