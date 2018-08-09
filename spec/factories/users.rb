83d5d38c61020435bf07c8636ed6fde9f4891a19
lim996 <simonestaffa96@gmail.com>
2018-08-09 11:20:33 +0200

    Added FakerGem to use Factory to create resources for test purpose.

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,20 @@
 FactoryBot.define do
-  factory :user do
-    
+  require 'faker'
+  factory :user do |u|
+    u.name = {Faker::Name.first_name}
+    u.surname = {Faker::Name.last_name}
+    u.username = {Faker::Internet.username(5..8)}
+    u.banned = false
+    u.email = {Faker::Internet.email}
+    u.encrypted_password = {Faker::Internet.password}
+    u.created_at = {Faker::Time.between(DateTime.now - 5, DateTime.now)}
+    u.updated_at = {Faker::Time.between(DateTime.now - 5, DateTime.now)}
+    u.avatar_image = {Faker::Avatar.image}
+    u.role = :user
+    u.uid = {Faker::Faker::Number.number(3)}
   end
+
+  #factory :devise_user do |u|
+
+  #end
 end
