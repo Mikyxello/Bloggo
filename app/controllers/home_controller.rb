3b735c77c04f9beac8b42ca5b7939713b91db48b
lim996 <simonestaffa96@gmail.com>
2018-08-16 11:12:42 +0200

    Blog and Posts limit

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,12 +2,12 @@ class HomeController < ApplicationController
 
   def index
     @blogs = if params[:blog]
-			         Blog.where('name LIKE ?',"%#{params[:blog]}%")
+			         Blog.where('name LIKE ?',"%#{params[:blog]}%").last(5)
 		         else
               Blog.all
 		              end
     @most_viewed = Blog.order('impressions_count DESC').last(5)
-    @posts = Post.all
+    @posts = Post.order('impressions_count DESC').last(5)
     @tags = ActsAsTaggableOn::Tag.most_used.order("taggings_count").first(5)
   end
 
